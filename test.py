ce599ce6478168c182a7bb9ce7492cea55a62bba
derf4bian <derf4bian@users.noreply.github.com>
2021-01-29 17:50:46 +0100

    layout check

---
+++ test.py
@@ -0,0 +1,16 @@
+# app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# defining a route
+@app.route("/", methods=['GET', 'POST', 'PUT']) # decorator
+def home(): # route handler function
+    # returning a response
+    return render_template("index.html")
+
+@app.route("/home")
+def second():
+    return render_template("home.html")
+
+app.run(debug = True) 
