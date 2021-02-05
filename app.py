e8fa0a77a1e7bc5e312db1e5fae7d3fc13147aad
derf4bian <derf4bian@users.noreply.github.com>
2021-02-05 19:09:11 +0100

    0.2

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # defining a route
-@app.route("/", methods=['GET', 'POST', 'PUT']) # decorator
+@app.route('/', methods=['GET', 'POST', 'PUT']) # decorator
 def home(): # route handler function
     # returning a response
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/home")
+@app.route('/home')
 def second():
-    return render_template("home.html")
+    return render_template('home.html')
 
-@app.route("/blue-fade")
+@app.route('/blue-fade')
 def bluefade():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/green-fade")
+@app.route('/green-fade')
 def greenfade():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/red-fade")
+@app.route('/red-fade')
 def redfade():
-    return render_template("index.html")
+    return render_template('index.html')
 
 app.run(debug = True)
